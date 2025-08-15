336fc52efcc1230f174a9fd21802ce35d7878deb
noturavrigk <ksm92038386@gmail.com>
2025-08-15 21:12:21 +0900

    First commit

---
+++ src/main/java/likelion/hackerthon/grocering/GroceringApplication.java
@@ -0,0 +1,13 @@
+package likelion.hackerthon.grocering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/hackerthon/grocering/GroceringApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.hackerthon.grocering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
