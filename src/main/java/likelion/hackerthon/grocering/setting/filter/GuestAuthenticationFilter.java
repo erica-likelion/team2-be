4ac38363b73fe023c57ba30b5f42af2c65a8795f
noturavrigk <ksm92038386@gmail.com>
2025-08-18 18:33:26 +0900

    [FEAT] add guest authenticaion filter

---
+++ src/main/java/likelion/hackerthon/grocering/setting/filter/GuestAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package likelion.hackerthon.grocering.setting.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class GuestAuthenticationFilter implements Filter {
+    private final List<String> WHITELIST = Arrays.asList(
+            "/guest/session",
+            "/renew/session"
+    );
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        HttpSession session = httpRequest.getSession(false);
+        String path = httpRequest.getRequestURI();
+
+        for (String whiteList : WHITELIST) {
+            if (path.equals(whiteList)) {
+                filterChain.doFilter(servletRequest, servletResponse);
+                return;
+            }
+        }
+
+        if (session == null) {
+            // 401 에러를 응답하고 중단
+            log.info("Guest Authentication Filter fail because session is null");
+            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Guest session required");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
