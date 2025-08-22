1d29f3f8c93466fab5aa8ded37c0e29c6fb74be4
xmoonanx <xmoonanx@gmail.com>
2025-08-22 14:39:53 +0900

    fix: cros 수정

--- src/main/java/likelion/hackerthon/grocering/setting/security/CorsConfig.java
+++ src/main/java/likelion/hackerthon/grocering/setting/security/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://team2-fe-hazel.vercel.app")
+                .allowedOrigins("http://localhost:3306", "https://grocering-demo.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
